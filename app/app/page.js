fdf05515b33f7b222eb571aa3f5914224688d8de
stumblechooch <stumblechooch@gmail.com>
2026-03-17 00:36:42 +0000

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main style={{ fontFamily: "Arial, sans-serif", padding: "40px" }}>
+      <h1>London Property Media</h1>
+      <p>Property photography, floor plans, EPC and compliance services.</p>
+      <p>If you can see this, the site is working.</p>
+    </main>
+  );
+}
